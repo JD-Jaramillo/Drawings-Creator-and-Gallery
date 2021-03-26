3cd2ee6d4b03886ef79845ec72d05e31bf29374f
basedmilz <basedmilz@gmail.com>
2021-03-26 12:16:02 -0400

    updated routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,6 +20,48 @@ router.get('/', withAuth, async (req, res) => {
     }
 });
 
+router.get('/drawing/:id', async (req, res) => {
+    try {
+        const drawingData = await Drawing.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const drawing = drawingData.get({ plain: true });
+
+        res.render('drawing', {
+            ...drawing,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/user', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: User }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('user', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/');
