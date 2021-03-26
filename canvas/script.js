06bc5c84dc64b79216da3232ecab08723ffb4000
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-26 17:31:52 -0400

    tool checks and funcitons

--- canvas/script.js
+++ canvas/script.js
@@ -5,13 +5,8 @@ window.addEventListener('load', () => {
     document.addEventListener('mousemove', sketch);
 });
 const canvas = document.querySelector('#canvas')
-
 const ctx = canvas.getContext('2d');
-
-
-
 let coord = { x: 0, y: 0 };
-
 let paint = false;
 
 function getPosition(event) {
@@ -21,11 +16,56 @@ function getPosition(event) {
 function startPainting(event) {
     paint = true;
     getPosition(event);
+    draw(event);
+}
+function draw() {
+    type = () => {
+        var radio = document.getElementsByName('tool');
+        console.log(radio);
+        for (var i = 0; i < radio.length; i++) {
+            radio[i].onclick = () => {
+                let choice = this.id;
+                console.log(choice);
+            }
+        }
+    };
+    let choice = type();
+    let size = parseInt(document.querySelector("#inputWidth").value);
+    let color = document.querySelector("#selColor").value;
+    let opacity = parseInt(document.querySelector("#inputOpacity").value);
+    //let stroke = parseInt(document.querySelector("#inputStorke").value);
+    let flow = parseInt(document.querySelector("#inputFlow").value);
+
+    if (choice = "pen") {
+        ctx.lineWidth = 3;
+        ctx.lineCap = "butt";
+        ctx.strokeStyle = color;
+    } else if (choice ="brush") {
+        ctx.lineWidth = size;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = color;
+    } else if (choice ="bucket") {
+        ctx.fillStyle = color;
+        ctx.fill();
+    } else if (choice ="eraser") {
+        ctx.globalCompositeOperation = 'destination-out';
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.lineWidth = size;
+        ctx.moveTo(old.x, old.y);
+        ctx.lineTo(coord.x, coord.y);
+        old = {x: x, y: y};
+    } else if (choice ="text") {
+        ctx.font = "${size}px Verdana";
+        ctx.strokeText();
+        ctx.strokeStyle = color;
+    }
+
 }
 function stopPainting() {
     paint = false;
 }
-
 function sketch(event) {
     if (!paint) return;
     ctx.beginPath();
@@ -34,7 +74,7 @@ function sketch(event) {
     ctx.strokeStyle = document.querySelector('#selColor').value;
     ctx.moveTo(coord.x, coord.y);
     getPosition(event);
-    ctx.lineTo(coord.x , coord.y);
+    ctx.lineTo(coord.x, coord.y);
     ctx.stroke();
 }
 function clearArea() {
