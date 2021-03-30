335e4045102c888154c2cc0ec399922fe3ed5d8b
Andrew Coon <thenran@gmail.com>
2021-03-30 12:56:15 -0400

    Fixed pic display in dashboard

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -14,6 +14,7 @@ router.get('/', withAuth, (req, res) => {
         attributes: [
             'id',
             'name',
+            'pic',
             'description',
             'date_created',
         ],
